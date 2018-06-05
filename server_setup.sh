59e62364b87151f43182fcb956438fc2b30ea2df
walidmujahid <walid.mujahid.dev@gmail.com>
2018-06-05 19:08:29 -0400

    update

--- server_setup.sh
+++ server_setup.sh
@@ -6,3 +6,7 @@ apt-get -y upgrade
 # Install Docker
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+# Install Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
