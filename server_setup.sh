a66f8ccdcf7d9b0b6ce77629c86576cab436c971
walidmujahid <walid.mujahid.dev@gmail.com>
2018-06-23 07:06:44 -0400

    update

--- server_setup.sh
+++ server_setup.sh
@@ -8,5 +8,7 @@ curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 
 # Install Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# command installs most recent stable release of docker Compose
+# taken from: https://stackoverflow.com/a/50730265/9905759
+curl -L https://github.com/docker/compose/releases/download/`curl -Ls -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest | awk -F / '{print $NF}'`/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
